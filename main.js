618e135416584d853107a2d6aa729e05d77e0bc7
RishiPoosarla <88695245+RishiPoosarla@users.noreply.github.com>
2022-04-28 19:19:39 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-//https://teachablemachine.withgoogle.com/models/CRH6aM9bd/
+//https://teachablemachine.withgoogle.com/models/F_hpvLByQ/
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/F_hpvLByQ/model.json", modelready);
+}
+function modelready(){
+    classifier.classify(gotresults);
+}
