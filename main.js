4c94d1527dbab52fbdc0e5daa60acae4f2d38cc4
RishiPoosarla <88695245+RishiPoosarla@users.noreply.github.com>
2022-05-10 18:34:45 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+dog = 0;
+cat = 0;
 //https://teachablemachine.withgoogle.com/models/F_hpvLByQ/
 function startClassification(){
     navigator.mediaDevices.getUserMedia({audio: true});
@@ -5,4 +7,41 @@ function startClassification(){
 }
 function modelready(){
     classifier.classify(gotresults);
+}
+
+function gotresults(error, results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+    r = Math.floor(Math.random() * 255) + 1;
+    g = Math.floor(Math.random() * 255) + 1;
+    b = Math.floor(Math.random() * 255) + 1;
+    document.getElementById("result_label").innerHTML = "I can Hear: " + results[0].label;
+    document.getElementById("result_label").style.color = "rgb(" + r + "," + g + "," + b + ")";
+    document.getElementById("result_count").innerHTML = "Detected Dog: " + dog + " Detected Cat: " + cat;
+    document.getElementById("result_count").style.color = "rgb(" + r + "," + g + "," + b + ")";
+  
+   img = document.getElementById("animal_img");
+    if(results[0].label == "Barking"){
+dog++;
+
+img.src = "dog.jpg";
+   }
+   else if(results[0].label == "Meowing"){
+    cat++;
+  
+    img.src = "cat image.jpg";
+       }
+       else if(results[0].label == "Roaring"){
+        img.src = "lion.jpg";
+           } 
+           else if(results[0].label == "Mooing"){
+            img.src = "cow.jpg";
+               } 
+               else{
+                   img.src = "ear.jpg";
+               }
+        }
 }
